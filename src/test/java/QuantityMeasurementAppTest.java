68645dd2770e35af71f5775259507d8af23b7def
asishvemparala11 <asishvemparala@gmail.com>
2026-04-17 10:19:27 +0530

    UC3: Implement Generic Quantity Class for DRY Principle

--- src/test/java/QuantityMeasurementAppTest.java
+++ src/test/java/QuantityMeasurementAppTest.java
@@ -68,4 +68,65 @@ public class QuantityMeasurementAppTest {
         QuantityMeasurementApp.Inches i1 = new QuantityMeasurementApp.Inches(1.0);
         assertTrue(i1.equals(i1));
     }
+
+    @Test
+    void testEquality_FeetToFeet_SameValue() {
+        QuantityMeasurementApp.Quantity q1 = new QuantityMeasurementApp.Quantity(1.0, QuantityMeasurementApp.LengthUnit.FEET);
+        QuantityMeasurementApp.Quantity q2 = new QuantityMeasurementApp.Quantity(1.0, QuantityMeasurementApp.LengthUnit.FEET);
+        assertTrue(q1.equals(q2));
+    }
+
+    @Test
+    void testEquality_InchToInch_SameValue() {
+        QuantityMeasurementApp.Quantity q1 = new QuantityMeasurementApp.Quantity(1.0, QuantityMeasurementApp.LengthUnit.INCH);
+        QuantityMeasurementApp.Quantity q2 = new QuantityMeasurementApp.Quantity(1.0, QuantityMeasurementApp.LengthUnit.INCH);
+        assertTrue(q1.equals(q2));
+    }
+
+    @Test
+    void testEquality_FeetToInches_EquivalentValue() {
+        QuantityMeasurementApp.Quantity q1 = new QuantityMeasurementApp.Quantity(1.0, QuantityMeasurementApp.LengthUnit.FEET);
+        QuantityMeasurementApp.Quantity q2 = new QuantityMeasurementApp.Quantity(12.0, QuantityMeasurementApp.LengthUnit.INCH);
+        assertTrue(q1.equals(q2));
+    }
+
+    @Test
+    void testEquality_InchToFeet_EquivalentValue() {
+        QuantityMeasurementApp.Quantity q1 = new QuantityMeasurementApp.Quantity(12.0, QuantityMeasurementApp.LengthUnit.INCH);
+        QuantityMeasurementApp.Quantity q2 = new QuantityMeasurementApp.Quantity(1.0, QuantityMeasurementApp.LengthUnit.FEET);
+        assertTrue(q1.equals(q2));
+    }
+
+    @Test
+    void testEquality_FeetToFeet_DifferentValue() {
+        QuantityMeasurementApp.Quantity q1 = new QuantityMeasurementApp.Quantity(1.0, QuantityMeasurementApp.LengthUnit.FEET);
+        QuantityMeasurementApp.Quantity q2 = new QuantityMeasurementApp.Quantity(2.0, QuantityMeasurementApp.LengthUnit.FEET);
+        assertFalse(q1.equals(q2));
+    }
+
+    @Test
+    void testEquality_InchToInch_DifferentValue() {
+        QuantityMeasurementApp.Quantity q1 = new QuantityMeasurementApp.Quantity(1.0, QuantityMeasurementApp.LengthUnit.INCH);
+        QuantityMeasurementApp.Quantity q2 = new QuantityMeasurementApp.Quantity(2.0, QuantityMeasurementApp.LengthUnit.INCH);
+        assertFalse(q1.equals(q2));
+    }
+
+    @Test
+    void testEquality_NullUnit() {
+        QuantityMeasurementApp.Quantity q1 = new QuantityMeasurementApp.Quantity(1.0, QuantityMeasurementApp.LengthUnit.FEET);
+        QuantityMeasurementApp.Quantity q2 = new QuantityMeasurementApp.Quantity(1.0, null);
+        assertFalse(q1.equals(q2));
+    }
+
+    @Test
+    void testEquality_QuantitySameReference() {
+        QuantityMeasurementApp.Quantity q1 = new QuantityMeasurementApp.Quantity(1.0, QuantityMeasurementApp.LengthUnit.FEET);
+        assertTrue(q1.equals(q1));
+    }
+
+    @Test
+    void testEquality_QuantityNullComparison() {
+        QuantityMeasurementApp.Quantity q1 = new QuantityMeasurementApp.Quantity(1.0, QuantityMeasurementApp.LengthUnit.FEET);
+        assertFalse(q1.equals(null));
+    }
 }
